7caf136212b6f64708ba263e862b4361417b8ecc
44ust14 <ustym.hanyk@ukr.net>
2017-04-30 15:01:29 +0300

    l20

---
+++ kola.js
@@ -0,0 +1,60 @@
+var x                                                   /* оголошення змінної */
+
+x = 5;                                                 /*ініціалізація змінної*/
+
+var y =10;                             /* оголошення змінної та ініціалізація */
+
+var resultAdd = x + y
+var resultSub = x - y
+var resultMult = x * y
+var resultDivide = x / y
+
+console.log( 'Додавання: ',resultAdd );
+console.log( 'Віднімання: ',resultSub );
+console.log( 'Множення: ',resultMult );
+console.log( 'Ділення: ',resultDivide );
+
+var firstNumber = Number(prompt('Enter first number'));
+var secondNumber = Number(prompt('Enter first number'));
+
+console.log('first number',firstNumber);
+console.log('second number',secondNumber);
+
+var add = firstNumber + secondNumber
+
+
+console.log('resultAdd', add);
+
+
+var sub = firstNumber - secondNumber
+
+console.log('resultSub',sub);
+
+
+var mult = firstNumber * secondNumber
+
+console.log('resultMult',mult);
+
+
+var divide = firstNumber / secondNumbe
+
+console.log('resultDivide',divide);
+
+var mod = firstNumber % secondNumber
+
+console.log('resultMod',mod);
+
+
+var inc = ++firstNumber
+console.log('resulInc',inc);
+
+
+var dec = --secondNumber
+console.log('resuDes',des);
+
+
+
+var txt1 = 'My first text';
+var txt2 = 'My second text';
+
+document.write(txt1 + '<br>' +txt2);
